2762939a288e1d58e01b56bebf87f9b9cb21c770
Marco Rodriguez <marcotbcreator@gmail.com>
2024-05-16 19:51:19 -0600

    refactor: Update mobile menu and resume button styles
    This commit updates the styles of the mobile menu and resume button in the MobileMenu component. The mobile menu now takes up the full height of the screen and the resume button has white text. These style changes enhance the visual appeal and user experience of the application.

--- src/components/ui/mobile-menu/MobileMenu.tsx
+++ src/components/ui/mobile-menu/MobileMenu.tsx
@@ -29,12 +29,12 @@ export const MobileMenu = () => {
             href={item.href}
             className="flex items-center mt-10 p-2 hover:bg-violet-700 rounded transition-all"
           >
-            <span className="ml-3 text-xl">{item.name}</span>
+            <span className="ml-3 text-xl text-twhite">{item.name}</span>
           </Link>
         ))}
         <Link href="https://drive.google.com/uc?export=download&id=1pOki3L2ewRbi2l7fKzHhpJq5h8Or8CZb">
           <button className="flex items-center mt-10 p-2 hover:bg-violet-700 rounded transition-all">
-            <span className="ml-3 text-xl">Resume</span>
+            <span className="ml-3 text-xl text-white">Resume</span>
           </button>
         </Link>
         <div className="w-full h-px bg-gray-200 my-5" />
