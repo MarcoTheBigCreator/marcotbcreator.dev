3a587a3d32655e4c8a30bf4cdbde05d87d3bb42f
Marco Rodriguez <marcotbcreator@gmail.com>
2024-05-20 02:46:20 -0600

    refactor: Update About component styles
    Update the About component styles to improve readability and accessibility. The text color of the headings is changed to white for better contrast against the background. This change enhances the visual presentation of the About section and improves the overall user experience.

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -8,7 +8,7 @@ export const About = () => {
   return (
     <div className="w-full h-full xl:h-[32rem] bg-black/[0.96] bg-grid-white/[0.07] antialiased grid grid-rows-3 lg:grid-rows-1 space-y-6 lg:grid-cols-3 lg:space-y-0 md:place-items-center px-10 ">
       {/* Bio component*/}
-      <section className="w-full h-auto lg:h-full md:w-11/12 xl:h-5/6 bg-zinc-900 border-[1px] border-white/20 rounded-3xl px-10 py-6">
+      <section className="w-full h-auto lg:h-full md:w-11/12 xl:h-5/6 bg-neutral-900 border-[1px] border-white/20 rounded-3xl px-10 py-6">
         <Bio />
       </section>
 
@@ -18,7 +18,7 @@ export const About = () => {
       </aside>
 
       {/* Skills & SoftSkills component */}
-      <aside className="w-full h-auto lg:h-full md:w-11/12 xl:h-5/6 bg-zinc-900 border-[1px] border-white/20 rounded-3xl px-10 py-6">
+      <aside className="w-full h-auto lg:h-full md:w-11/12 xl:h-5/6 bg-neutral-900 border-[1px] border-white/20 rounded-3xl px-10 py-6">
         <section className="w-full flex flex-col mb-6">
           <Skills />
         </section>
