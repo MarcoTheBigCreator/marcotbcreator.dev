eda1f75cfeaaf77cbc224882618ab48f920087cc
Marco Rodriguez <marcotbcreator@gmail.com>
2024-05-20 21:29:13 -0600

    refactor: Update About component styles
    Improve readability and accessibility by changing the text color of headings to white for better contrast against the background. Enhance the visual presentation of the About section and improve the overall user experience.

--- src/components/about/Timeline.tsx
+++ src/components/about/Timeline.tsx
@@ -20,11 +20,11 @@ export const Timeline = ({ timelineTitle, info }: Props) => {
           <div className="font-semibold text-xl text-white mb-1 sm:mb-0">
             {item.title}
           </div>
-          <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-neutral-600 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-violet-600 after:border-4 after:box-content after:border-neutral-900 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
-            <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-neutral-100 bg-violet-500 rounded-full">
+          <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-neutral-600 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-violet-700 after:border-4 after:box-content after:border-neutral-900 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
+            <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-neutral-100 bg-violet-700 rounded-full">
               {item.date}
             </time>
-            <div className="text-lg font-medium text-violet-600">
+            <div className="text-lg font-medium text-violet-500">
               {item.place}
             </div>
           </div>
