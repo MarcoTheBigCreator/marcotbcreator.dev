df250b1fdc856796b3d600479ea3e3aa81c49e44
Marco Rodriguez <marcotbcreator@gmail.com>
2024-05-22 13:58:18 -0600

    refactor: Update global CSS to include gradient mask on body element

--- src/components/ui/footer/Footer.tsx
+++ src/components/ui/footer/Footer.tsx
@@ -7,7 +7,7 @@ import { TfiLinkedin } from 'react-icons/tfi';
 
 export const Footer = () => {
   return (
-    <div className="dashes bg-black">
+    <div className="dashes">
       <footer className="grid grid-cols-1 lg:grid-cols-3 gap-4 bg-black px-20 py-6 text-white center w-full items-center">
         {/* Name and logo */}
         <section className="hidden lg:inline-grid space-y-2">
