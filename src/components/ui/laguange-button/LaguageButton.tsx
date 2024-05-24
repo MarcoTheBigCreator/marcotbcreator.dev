9a7bba5c44a9de1e2b7a6376780035e418a14903
Marco Rodriguez <marcotbcreator@gmail.com>
2024-05-23 23:50:03 -0600

    refactor: Update language buttons to use button element instead of link
    Update the LaguageButton and LaguageButtonMobile components to use button elements instead of link elements for improved accessibility and usability. This ensures that the language buttons can be easily interacted with using keyboard navigation and assistive technologies.

--- src/components/ui/laguange-button/LaguageButton.tsx
+++ src/components/ui/laguange-button/LaguageButton.tsx
@@ -2,6 +2,7 @@
 
 import { useUIStore } from '@/store';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
 interface Props {
@@ -11,31 +12,43 @@ interface Props {
 }
 
 export const LaguageButton = ({ icon, text, href }: Props) => {
+  const router = useRouter();
+
+  const onHandleLocale = (href: string) => {
+    router.replace(href);
+  };
+
   return (
-    <Link
-      href={href}
+    <button
+      onClick={() => onHandleLocale(href)}
       className="pt-3 h-12 w-12 flex align-middle justify-center shadow-[0_5px_15px_1px_rgb(140,0,255,55%)] hover:shadow-[0_6px_20px_rgba(140,0,255,30%)] hover:bg-violet-900 bg-violet-700 rounded-full text-white font-semibold lg:font-medium transition duration-200 ease-linear"
     >
       {icon && <span className="mr-1">{icon}</span>}
       {text}
-    </Link>
+    </button>
   );
 };
 
 export const LaguageButtonMobile = ({ icon, text, href }: Props) => {
+  const router = useRouter();
+
   const isSideMenuOpen = useUIStore((state) => state.isSideMenuOpen);
 
   useEffect(() => {
     document.body.style.overflow = isSideMenuOpen ? 'hidden' : 'auto';
   }, [isSideMenuOpen]);
+
+  const onHandleLocaleMobile = (href: string) => {
+    useUIStore.setState({ isSideMenuOpen: false });
+    router.replace(href);
+  };
   return (
-    <Link
-      href={href}
+    <button
+      onClick={() => onHandleLocaleMobile(href)}
       className="pt-3 h-12 w-12 flex align-middle justify-center shadow-[0_5px_15px_1px_rgb(140,0,255,55%)] hover:shadow-[0_6px_20px_rgba(140,0,255,30%)] hover:bg-violet-900 bg-violet-700 rounded-full text-white font-semibold lg:font-medium transition duration-200 ease-linear"
-      onClick={() => useUIStore.setState({ isSideMenuOpen: false })}
     >
       {icon && <span className="mr-1">{icon}</span>}
       {text}
-    </Link>
+    </button>
   );
 };
