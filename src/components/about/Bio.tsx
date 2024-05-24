30e666f2336710442dbaf207a5bb203385ac1c5c
Marco Rodriguez <marcotbcreator@gmail.com>
2024-05-24 01:10:30 -0600

    refactor: Update Bio component to use translated titles
    Update the Bio component to use translated titles for the bio information fields. This ensures consistency and improves the localization of the component.

--- src/components/about/Bio.tsx
+++ src/components/about/Bio.tsx
@@ -1,16 +1,41 @@
 import { titleFont } from '@/config/fonts';
 import { useTranslations } from 'next-intl';
 
+interface BioInfo {
+  name: string;
+  profession: string;
+  from: string;
+  live_in: string;
+  age: string;
+}
+
+interface BioInfoTitles {
+  nameTitle: string;
+  professionTitle: string;
+  fromTitle: string;
+  live_inTitle: string;
+  ageTitle: string;
+}
+
 export const Bio = () => {
   const t = useTranslations('bioInfo');
+  const bioTitles = useTranslations('bioTitles');
   const interestTitle = useTranslations('titles');
 
-  const bioInfo = {
+  const bioInfo: BioInfo = {
     name: t('name'),
     profession: t('profession'),
     from: t('from'),
-    lives_in: t('lives_in'),
+    live_in: t('live_in'),
     age: t('age'),
+  };
+
+  const bioInfoTitles: BioInfoTitles = {
+    nameTitle: bioTitles('nameTitle'),
+    professionTitle: bioTitles('professionTitle'),
+    fromTitle: bioTitles('fromTitle'),
+    live_inTitle: bioTitles('live_inTitle'),
+    ageTitle: bioTitles('ageTitle'),
   };
 
   return (
@@ -28,20 +53,24 @@ export const Bio = () => {
         </span>
       </h3>
       <ul className="list-disc ml-5 text-violet-400/90 font-normal text-base space-y-1 capitalize">
-        {Object.entries(bioInfo).map(([key, value]) => {
+        {Object.keys(bioInfo).map((key) => {
           if (key === 'name' || key === 'profession') return null;
+          const titleKey = `${key}Title` as keyof BioInfoTitles;
+          const title = bioInfoTitles[titleKey];
           return (
             <li key={key}>
-              <b>{key.replace(/_/g, ' ')}:</b>{' '}
-              <span className="text-white">{value}</span>
+              <b>{title ? title.replace(/_/g, ' ') : ''}:</b>{' '}
+              <span className="text-white">
+                {bioInfo[key as keyof BioInfo]}
+              </span>
             </li>
           );
         })}
         <li>
           <b>{interestTitle('interestTitle')}:</b>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 gap-1 text-white ml-1 mt-1">
-            {t.raw('interests').map((interest: string) => (
-              <p key={interest}>- {interest}</p>
+            {t.raw('interests').map((interest: string, index: number) => (
+              <p key={index}>- {interest}</p>
             ))}
           </div>
         </li>
